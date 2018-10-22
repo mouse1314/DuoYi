bb35efe0eaec3c16d30eab3c730d0fbbc233a335
4-3116005176-洪崇伟 <705393356@qq.com>
2018-10-22 20:36:54 +0800

    上传部分修改

--- duoyi/src/main/java/com/duoyi/web/controller/LoginController.java
+++ duoyi/src/main/java/com/duoyi/web/controller/LoginController.java
@@ -1,8 +1,11 @@
 package com.duoyi.web.controller;
 
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.duoyi.model.vo.UserVo;
 
 import net.sf.json.JSONObject;
 
@@ -10,8 +13,12 @@ public class LoginController {
 
 	@RequestMapping(value = "/login", method = RequestMethod.POST)
     @ResponseBody
-    public JSONObject login(){
+    public JSONObject login(@RequestBody UserVo user){
+		JSONObject json = new JSONObject();
 		
-		return null;
+		
+		
+		
+		return json;
 	}
 }
