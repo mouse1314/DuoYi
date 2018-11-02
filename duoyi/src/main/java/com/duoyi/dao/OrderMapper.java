b4a00f23260f154d39bb9293ca167b8e8a744c19
linhao <linhao0056@qq.com>
2018-11-02 20:42:36 +0800

    完成订单接口

---
+++ duoyi/src/main/java/com/duoyi/dao/OrderMapper.java
@@ -0,0 +1,22 @@
+package com.duoyi.dao;
+
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import com.duoyi.model.po.Order;
+import com.duoyi.model.vo.OrderVo;
+
+
+/**
+ * @author 浩子
+ *2018年11月2日
+ */
+@Repository
+public interface OrderMapper {
+
+	public int insertOrder(Order order);
+	public int delectOrder(int id);
+	public OrderVo selectOrderById(int id);
+	public List<OrderVo> selectOrders(int userid);
+}
---
+++ duoyi/src/main/java/com/duoyi/web/service/OrderService.java
@@ -0,0 +1,18 @@
+package com.duoyi.web.service;
+
+import java.util.List;
+
+import com.duoyi.model.po.Order;
+import com.duoyi.model.vo.OrderVo;
+
+/**
+ * @author 浩子
+ *2018年11月2日
+ */
+public interface OrderService {
+
+	public int insertOrder(Order order);
+	public int delectOrder(int id);
+	public OrderVo selectOrderById(int id); 
+	public List<OrderVo> selectOrders(int userid);
+}
